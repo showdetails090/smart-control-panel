73874cb4238a376ede332e3ff976238d76d7e0a4
showdetails090 <showdetails090@users.noreply.github.com>
2025-11-15 17:44:11 +0000

    Add new UI components: CardSpotlight, GlareCard, and CanvasRevealEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { DotBackgroundDemo } from "./components/ui/DotBackground";
 import { FloatingDockDemo } from "./components/ui/FloatingDockDemo";
+import { GlareCardDemo } from "./components/ui/glare-card-component";
+import { CardSpotlightDemo } from "./components/ui/CardSpotlightcomponent";
 function App() {
   
 
   return (
     <>
+    <div>
       <DotBackgroundDemo />
+      <CardSpotlightDemo />
+      </div>
       <FloatingDockDemo />
+      <br /><br /><br /><br />br
+      <br /><br /><br /><br />br
     </>
   )
 }
